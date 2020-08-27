44bba085a6d92cffead8499c1a6ceab4a3e75978
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-08-28 00:55:01 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -el
 
-wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/1.1.0/dependency-tree-diff.jar"
+wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/1.1.0/dependency-tree-diff.jar" -q -O dependency-tree-diff.jar
 ls -l
 
-diff=$(java -jar dependency-tree-diff.jar "$INPUT_BASEREF" "$INPUT_HEADREF")
+old="$INPUT_BASEREF"
+new="$INPUT_HEADREF"
+
+diff=$(java -jar dependency-tree-diff.jar)
 echo "::set-output name=text-diff::$diff"
