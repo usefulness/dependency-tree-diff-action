2623b4d59a2a54de483dcf1cb43fcbf5ff841888
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-08-28 00:55:15 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -el
 
-wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/1.1.0/dependency-tree-diff.jar" -q -O dependency-tree-diff.jar
+wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/1.1.0/dependency-tree-diff.jar" -q
 ls -l
 
 old="$INPUT_BASEREF"
