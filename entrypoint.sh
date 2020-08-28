55e052c01283f9d9bd6752ae6a4dfd668c9a2d98
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-08-28 17:19:08 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/1.1.0/dependency-tree-diff.jar" -q -O dependency-tree-diff.jar
 ls -l
 
-echo git status
+cd app
+git checkout "$INPUT_BASEREF"
+./gradlew :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >../old.txt
+git checkout "$INPUT_HEADREF"
+./gradlew :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >../new.txt
+cd ..
 
-old="$INPUT_BASEREF"
-new="$INPUT_HEADREF"
-
-diff=$(java -jar dependency-tree-diff.jar)
+diff=$(java -jar dependency-tree-diff.jar old.txt new.text)
 echo "::set-output name=text-diff::$diff"
