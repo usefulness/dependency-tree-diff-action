4dd3c15eeb4881a4def43139277b6aa30c8d81a4
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-08-28 19:16:52 +0200

    Initial version

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -el
+
+wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/1.1.0/dependency-tree-diff.jar" -q -O dependency-tree-diff.jar
+
+./gradlew project
+./gradlew :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >new_diff.txt
+git checkout "$INPUT_BASEREF"
+./gradlew :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >old_diff.txt
+
+diff=$(java -jar dependency-tree-diff.jar old_diff.txt new_diff.txt)
+diff="${diff//'%'/'%25'}"
+diff="${diff//$'\n'/'%0A'}"
+diff="${diff//$'\r'/'%0D'}"
+echo "::set-output name=text-diff::$diff"
