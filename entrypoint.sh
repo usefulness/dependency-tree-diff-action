3721e572003ebf22afcb5f962cf1e53d34799bcc
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2022-08-11 09:31:12 +0200

    Add ability to pass additional arguments to gradle invocation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ cd "$INPUT_BUILD_ROOT_DIR"
 
 wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/$INPUT_VERSION/dependency-tree-diff.jar" -q -O dependency-tree-diff.jar
 
-./gradlew projects
-./gradlew :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >new_diff.txt
+./gradlew $ADDITIONAL_GRADLE_ARGUMENTS projects
+./gradlew $ADDITIONAL_GRADLE_ARGUMENTS :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >new_diff.txt
 git switch --force "$INPUT_BASEREF"
-./gradlew :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >old_diff.txt
+./gradlew $ADDITIONAL_GRADLE_ARGUMENTS :"$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >old_diff.txt
 
 diff=$(java -jar dependency-tree-diff.jar old_diff.txt new_diff.txt)
 diff="${diff//'%'/'%25'}"
