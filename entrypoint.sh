37b087b81420986636fd3bfeb14b75afba7a37a5
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2023-10-30 15:17:11 +0100

    Quick workaround for root project passed as project parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ cd "$INPUT_BUILD_ROOT_DIR"
 
 wget "https://github.com/JakeWharton/dependency-tree-diff/releases/download/$INPUT_VERSION/dependency-tree-diff.jar" -q -O dependency-tree-diff.jar
 
+if [ "$INPUT_PROJECT" == ":" ]; then
+  INPUT_PROJECT=""
+fi
+
 ./gradlew $ADDITIONAL_GRADLE_ARGUMENTS projects
 ./gradlew $ADDITIONAL_GRADLE_ARGUMENTS "$INPUT_PROJECT":dependencies --configuration "$INPUT_CONFIGURATION" >new_diff.txt
 git fetch --force origin "$INPUT_BASEREF":"$INPUT_BASEREF" --no-tags
